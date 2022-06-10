f8e4377ec5c4f0c9f810a6274f7d464f487ecf3b
MikyGus <mikygus@gmail.com>
2022-06-10 10:24:38 +0200

    Added more tests on different directions

---
+++ 2048/2048.Tests/Fixtures/GridFixturesOriginalAndResult.cs
@@ -0,0 +1,99 @@
+﻿namespace _2048.Tests.Fixtures
+{
+    internal class GridFixturesOriginalAndResult
+    {
+        public static int[,] Start(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return new int[,]
+                    {
+                        { 2, 4, 2, 1024 },
+                        { 0, 16, 64, 1024 },
+                        { 0, 8, 32, 64 },
+                        { 2, 2, 4, 0 }
+                    };
+
+                default:
+                    return new int[,]
+                    {
+                        { 2, 2, 4, 4 },
+                        { 0, 4, 2, 2 },
+                        { 0, 0, 0, 2 },
+                        { 2, 0, 0, 2 }
+                    };
+            }
+        }
+
+        public static int[,] Result(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return new int[,]
+                    {
+                        { 4, 4, 2, 2048 },
+                        { 0, 16, 64, 64 },
+                        { 0, 8, 32, 0 },
+                        { 0, 2, 4, 0 }
+                    };
+                case 1:
+                    return new int[,]
+                    {
+                        { 2, 4, 2, 1024 },
+                        { 16, 64, 1024, 0 },
+                        { 8, 32, 64, 0 },
+                        { 4, 4, 0, 0 }
+                    };
+                case 2:
+                    return new int[,]
+                    {
+                        { 0, 4, 2, 0 },
+                        { 0, 16, 64, 0 },
+                        { 0, 8, 32, 2048 },
+                        { 4, 2, 4, 64 }
+                    };
+                case 3:
+                    return new int[,]
+                    {
+                        { 2, 4, 2, 1024 },
+                        { 0, 16, 64, 1024 },
+                        { 0, 8, 32, 64 },
+                        { 0, 0, 4, 4 }
+                    };
+                default:
+                    return new int[,]
+                    {
+                        { 2, 2, 4, 4 },
+                        { 0, 4, 2, 2 },
+                        { 0, 0, 0, 2 },
+                        { 2, 0, 0, 2 }
+                    };
+            }
+        }
+
+        private static int[,] Result_01(int direction)
+        {
+            switch (direction)
+            {
+                case 0:
+                    return new int[,]
+                    {
+                        { 4, 4, 2, 2048 },
+                        { 0, 16, 64, 64 },
+                        { 0, 8, 32, 0 },
+                        { 0, 2, 4, 0 }
+                    };
+                default:
+                    return new int[,]
+                    {
+                        { 4, 4, 2, 2048 },
+                        { 0, 16, 64, 64 },
+                        { 0, 8, 32, 0 },
+                        { 0, 2, 4, 0 }
+                    };
+            }
+        }
+    }
+}
---
+++ 2048/2048.Tests/System/Grid/GridNavigatorTests.cs
@@ -0,0 +1,82 @@
+﻿using _2048.Grid;
+using _2048.Grid.MoveDirection;
+using _2048.Tests.Fixtures;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace _2048.Tests.System.Grid
+{
+    public class GridNavigatorTests
+    {
+
+        [Fact]
+        public void MoveGridInDirection_ShouldFixtureLeft_WhenMoveDirectionIsLeft()
+        {
+            // Arrange
+            int GridSize = 4;
+            int Index = 0;
+            IMoveDirection moveDirection = new MoveDirectionLeft(GridSize);
+            IGridArea grid = new GridArea(GridFixturesOriginalAndResult.Start(Index));
+            IGridNavigator sut = new GridNavigator(grid);
+            // Act
+            sut.MoveGridInDirection(moveDirection);
+
+            // Assert
+            sut.GetGridArea().Grid.Should().BeEquivalentTo(GridFixturesOriginalAndResult.Result(Index));
+        }
+
+        [Fact]
+        public void MoveGridInDirection_ShouldReturnFixtureUP_WhenMoveDirectionIsUp()
+        {
+            // Arrange
+            int GridSize = 4;
+            int IndexStart = 0;
+            int IndexResult = 1;
+            IMoveDirection moveDirection = new MoveDirectionUp(GridSize);
+            IGridArea grid = new GridArea(GridFixturesOriginalAndResult.Start(IndexStart));
+            IGridNavigator sut = new GridNavigator(grid);
+            // Act
+            sut.MoveGridInDirection(moveDirection);
+
+            // Assert
+            sut.GetGridArea().Grid.Should().BeEquivalentTo(GridFixturesOriginalAndResult.Result(IndexResult));
+        }
+
+        [Fact]
+        public void MoveGridInDirection_ShouldReturnFixtureRight_WhenMoveDirectionIsRight()
+        {
+            // Arrange
+            int GridSize = 4;
+            int IndexStart = 0;
+            int IndexResult = 2;
+            IMoveDirection moveDirection = new MoveDirectionRight(GridSize);
+            IGridArea grid = new GridArea(GridFixturesOriginalAndResult.Start(IndexStart));
+            IGridNavigator sut = new GridNavigator(grid);
+            // Act
+            sut.MoveGridInDirection(moveDirection);
+
+            // Assert
+            sut.GetGridArea().Grid.Should().BeEquivalentTo(GridFixturesOriginalAndResult.Result(IndexResult));
+        }
+
+        [Fact]
+        public void MoveGridInDirection_ShouldReturnFixtureDown_WhenMoveDirectionIsDown()
+        {
+            // Arrange
+            int GridSize = 4;
+            int IndexStart = 0;
+            int IndexResult = 3;
+            IMoveDirection moveDirection = new MoveDirectionDown(GridSize);
+            IGridArea grid = new GridArea(GridFixturesOriginalAndResult.Start(IndexStart));
+            IGridNavigator sut = new GridNavigator(grid);
+            // Act
+            sut.MoveGridInDirection(moveDirection);
+
+            // Assert
+            sut.GetGridArea().Grid.Should().BeEquivalentTo(GridFixturesOriginalAndResult.Result(IndexResult));
+        }
+    }
+}
